fd267697e439e18b5d3d54d1bd2e4edda42cc144
Maxim Vainikka <rithmique@yahoo.com>
2021-05-04 06:25:09 +0200

    Initial Commit

---
+++ MoonshotUI/MoonshotUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotUIApp.swift
+//  MoonshotUI
+//
+//  Created by Максим Нуждин on 04.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
